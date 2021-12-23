f242a19579b0aa6772cf28dca28cdb1465906fb5
Pratik Choudhuri <pratikchoudhuri1609@gmail.com>
2021-12-23 12:29:28 +0530

    Added and modified database models

---
+++ README.md
@@ -0,0 +1,3 @@
+<!-- Add Sem Marks Cards (8) (Boolean field Recieved/Pending) (Backlog Certificate) -->
+<!-- AddOn Courses Certificates (Both Document and coursename) -->
+<!-- University Certificate -->
--- main/models.py
+++ main/models.py
@@ -1,11 +1,29 @@
+# Django Modules
 from typing import Match
 from django.db import models
+
+# Pip Modules
 import datetime 
 
-# Create your models here.
+# Custom Modules
+from .validators import validate_file_extension
+
+
+class Course(models.Model):
+    coursename = models.CharField(max_length=100)
+
+    def __repr__(self) -> str:
+        return self.coursename
+
+    def __str__(self) -> str:
+        return self.coursename
 
 class Student(models.Model):
     name = models.CharField(max_length=100)
+    course_enrolled = models.ForeignKey(Course, on_delete=models.CASCADE)
+    registration_number_or_usn_number = models.CharField(max_length=100, blank=True)
+    
+    
     age = models.IntegerField(default=0)
     dob = models.DateField(blank=True, default=datetime.date.today())
 
@@ -18,10 +36,20 @@ class Student(models.Model):
 
     blood_group = models.CharField(max_length=3, blank=True)
 
+    religion = models.CharField(max_length=100, blank=True)
+    caste = models.CharField(max_length=100, blank=True)
+
+    batch = models.DurationField(null=True)
+    semester = models.IntegerField(default=0)
+
+
     def __repr__(self) -> str:
         return self.name
 
     def __str__(self) -> str:
+        return self.name
+
+    def get_name(self):
         return self.name
 
 class Parents(models.Model):
@@ -34,3 +62,26 @@ class Parents(models.Model):
 
     parent_contact_number_1 = models.IntegerField(default=0000000000)
     parent_contact_number_2 = models.IntegerField(default=0000000000)
+
+    def __repr__(self) -> str:
+        return self.student.name + " Parents"
+
+    def __str__(self) -> str:
+        return self.student.name + " Parents"
+
+class Document(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+
+    class_10_result = models.FileField(upload_to=f"documents/10", validators=[validate_file_extension], blank=True)
+    class_12_result = models.FileField(upload_to=f"documents/12", validators=[validate_file_extension], blank=True)
+
+    PAN_Number = models.CharField(max_length=10)
+    Adhaar_Number = models.BigIntegerField(default=0000000000000)
+
+    def __repr__(self) -> str:
+        return self.student.name + "'s Documents"
+
+    def __str__(self) -> str:
+        return self.student.name + "'s Documents"
+
+
