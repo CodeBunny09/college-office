af0f24d5f1981af8e368a965f19f5d188a760809
Pratik Choudhuri <pratikchoudhuri1609@gmail.com>
2021-12-23 10:58:39 +0530

    Commit 2, Changed the database models

--- main/models.py
+++ main/models.py
@@ -1,12 +1,36 @@
+from typing import Match
 from django.db import models
+import datetime 
 
 # Create your models here.
 
 class Student(models.Model):
     name = models.CharField(max_length=100)
+    age = models.IntegerField(default=0)
+    dob = models.DateField(blank=True, default=datetime.date.today())
+
+    address1 = models.CharField(max_length=500, blank=True)
+    address2 = models.CharField(max_length=500, blank=True)
+
+    email = models.EmailField(blank=True)
+    contact_number_1 = models.BigIntegerField(default=0000000000)
+    contact_number_2 = models.BigIntegerField(default=0000000000)
+
+    blood_group = models.CharField(max_length=3, blank=True)
 
     def __repr__(self) -> str:
         return self.name
 
     def __str__(self) -> str:
         return self.name
+
+class Parents(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    father_name = models.CharField(max_length=100)
+    mother_name = models.CharField(max_length=100)
+
+    father_email = models.EmailField(blank=True)
+    mother_email = models.EmailField(blank=True)
+
+    parent_contact_number_1 = models.IntegerField(default=0000000000)
+    parent_contact_number_2 = models.IntegerField(default=0000000000)
